2aa176f3783194584b00c2a2d1d96289addc183e
Louell Sala <louell_sala@yahoo.com>
2022-03-24 19:52:22 -0400

    added Landing Page

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Home from "./Home";
+import Name from "./Components/Name";
+import ParticleBackground from "./Components/ParticleBackground";
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+  <div className="App">
+
+<BrowserRouter>
+    <Routes>
+       <Route
+            path="/Home"
+            element={
+            <Home />} />
+        <Route
+            path="/"
+            element={
+            <ParticleBackground  />} />
+       
+    </Routes>
+</BrowserRouter> 
+
+
+
+
+  </div>
   );
 }
 
---
+++ src/Components/Name.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const Name = () => {
+    return (  
+        <div class="center">
+          <h1 style={{ color: 'whitesmoke' }}>  START HERE </h1> 
+          
+        </div>
+    );
+}
+ 
+
+export default Name;
